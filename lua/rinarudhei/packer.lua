08e4649c09dbbe1a0a27c89b2258532d858521b7
rinarudhei <rinaldiadrian5@gmail.com>
2025-02-20 23:51:41 +0700

    add lualine

--- lua/rinarudhei/packer.lua
+++ lua/rinarudhei/packer.lua
@@ -88,7 +88,8 @@ return require('packer').startup(function(use)
     }
     use {
         'nvim-lualine/lualine.nvim',
-        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+        'archibate/lualine-time',
+        requires = {'nvim-tree/nvim-web-devicons', opt = true},
     }
 
 end)
